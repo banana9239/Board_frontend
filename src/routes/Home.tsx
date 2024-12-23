b0630498dc76d2bfd44df1bdf2ca7baae72ae505
banao <banao@banaos-MacBook-Air.local>
2024-12-23 17:32:53 +0900

    20241223 post image form

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,11 @@
-import { Box, Button} from "@chakra-ui/react";
+import { Box, Button, Text} from "@chakra-ui/react";
 import CategoryList from "../components/CategoryList";
 import { useQuery } from "@tanstack/react-query";
 import { getboards, getLargeCategories, getMediumCategories, getPosts, getSmallCategories } from "../api";
 import { Link, useParams } from "react-router-dom";
 import PostList from "../components/PostList";
 import { useEffect, useState } from "react";
+import useUser from "../lib/useUser";
 
 
 export default function Home() {  
@@ -53,9 +54,25 @@ export default function Home() {
     }
   }
   const [beforeUrl, setBeforeUrl] = useState("");
+
+  const [pageSize, setPageSize] = useState(5);
+  const [count, setCount] = useState(50);
+  const [page, setPage] = useState(1)
+  const items = new Array(count).fill(0).map((_, index) => `Lorem ipsum dolor sit amet ${index + 1}`)
+
+  const startRange = (page - 1) * pageSize
+  const endRange = startRange + pageSize
+
+  const visibleItems = items.slice(startRange, endRange);
+
+  const {user} = useUser();
+
   useEffect(() => {
     setBeforeUrl(`/${largePk}/${mediumPk}/${smallPk}/${boardPk}`);
+
+    
   },[boardPk]);
+  
 
   return (
     <Box>
@@ -65,8 +82,15 @@ export default function Home() {
         board_name={board_name}
         beforeUrl={beforeUrl}
       />
+      <Box py={5} px={10} width="100%">
+      {visibleItems.map((item, index) => {
+        return (<>
+          <Text display="flex" justifyContent="flex-end">{index}</Text>
+          </>)})}
+      </Box>
       <Box width={"100%"} display="flex" justifyContent="flex-end" py={5} px={10}>
-        <Link to={"/post/editor"} state={{boardPk:boardPk, }}><Button ml="auto">글쓰기</Button></Link>
+        {!user ? "":<Link to={"/post/editor"} state={{boardPk:boardPk, }}><Button ml="auto">글쓰기</Button></Link>}
+        
       </Box>
     </Box>
   );
--- src/routes/PostForm.tsx
+++ src/routes/PostForm.tsx
@@ -1,16 +1,18 @@
-import { Box, Button, Input, Text, VStack, FormControl, FormHelperText, FormLabel, Textarea, Select  } from "@chakra-ui/react";
+import { Box, Button, Input, Text, VStack, FormControl, FormHelperText, FormLabel, Textarea, Select, LightMode, Image  } from "@chakra-ui/react";
 import ProtectPage from "../components/ProtectPage";
 import { useForm } from "react-hook-form";
 import useUser from "../lib/useUser";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { getboard, posting } from "../api";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 interface IPostProps {
     title: string,
     content: string,
     sortation: string,
     board:string
+    file: FileList,
 }
 
 interface IBoardProps {
@@ -22,10 +24,11 @@ interface IBoardProps {
     board_category: number
 }
 
+type IImage = string[];
 
 export default function PostForm() {
     const { register, handleSubmit } = useForm<IPostProps>();
-    const {user} = useUser();
+    
     const location = useLocation();
     const {boardPk} = location.state||null;
     const { data: boardData } = useQuery<IBoardProps>({
@@ -52,6 +55,26 @@ export default function PostForm() {
         console.log(data);
         mutation.mutate(data)
     }
+
+    const [image, setImage] = useState<IImage>();
+    
+    const handleImage = (e: any) => {
+        if (!e.target.files) return;
+        const files = e.target.files;
+        console.log(e.target.files);
+        if (files){
+            let images = [];
+            for (let i=0; i<files.length; i++){
+                images.push(window.URL.createObjectURL(files[i]));
+            }
+            setImage(images);
+        }
+    }
+
+    useEffect(() => {
+        console.log(image);
+      },[image]);
+
     return (
         <ProtectPage>
             <Box py={5} px={10} width={"100%"} height={"400px"}>
@@ -64,7 +87,7 @@ export default function PostForm() {
                     </FormControl>
                     <FormControl>
                         <FormLabel>이름</FormLabel>
-                        <Input isDisabled value={user.name}/> 
+                        <Input isDisabled value={"dff"}/> 
                     </FormControl>
                     <FormControl>
                         <FormLabel>제목</FormLabel>
@@ -76,8 +99,16 @@ export default function PostForm() {
                         <FormLabel>내용</FormLabel>
                         <Textarea {...register("content",{
                             required:true
-                        })}/>                         
+                        })}/>
+                        <Input mb={5} multiple type="file" accept="image/*" {...register("file", {})} onChange={handleImage}/>
+                        <Box >
+                        {image ? image.map((ima) => (<>
+                            <Image src={ima} width={"250px"} height={"250px"} alt="불만있냐?"/>
+                            <Textarea  mb={5} placeholder="미디어에 대한 설명을 쓰시오."/>
+                            </>)):""}
+                        </Box>
                     </FormControl>
+                    
                     <FormControl>
                         <FormLabel>종류</FormLabel>
                         <Select
@@ -88,14 +119,16 @@ export default function PostForm() {
                             <option value="자유">자유</option>
                         </Select>
                     </FormControl>
-                    <Button mt={10}
-                        type="submit"
-                        colorScheme={"red"}
-                        size="lg"
-                        w="100%"
-                        >
-                        posting!
-                    </Button>
+                    <LightMode>
+                        <Button mt={10}
+                            type="submit"
+                            colorScheme={"red"}
+                            size="lg"
+                            w="100%"
+                            >
+                            posting!
+                        </Button>
+                    </LightMode>
                 </VStack>
             </Box>
         </ProtectPage>
